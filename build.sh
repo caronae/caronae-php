6f3ab58a8d0c7b8d671909f4ae984b5b97dec2e9
Mario Cecchi <macecchi@gmail.com>
2018-09-04 00:28:37 -0300

    add logs to docker push script

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+set -e
+echo "$DOCKER_PUSH"
 
 docker build -t caronae/php:latest .
 
-if [ "$DOCKER_PUSH" = 'true' ]; then
+if [ "$DOCKER_PUSH" == 'true' ]; then
   docker login -u $DOCKER_USER -p $DOCKER_PASS
   docker push caronae/php
 fi
-
